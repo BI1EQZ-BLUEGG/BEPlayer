6eaeba41e469a83c2d6cf07f4ec13d64c7df6ea6
bluegg <egg@bluegg.fun>
2024-12-07 13:08:10 +0800

    @feat 调整结构；

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,28 +9,27 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "BEPlayer",
-            targets: ["BEPlayer"]),
+            targets: ["BEPlayer"]
+        ),
         .library(
             name: "BELoader",
             targets: ["BELoader"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        // BEPlayer 模块
         .target(
             name: "BEPlayer",
             path: "Sources/BEPlayer",
-            publicHeadersPath: "include"
+            publicHeadersPath: "include" // Objective-C 头文件目录
         ),
+        // BELoader 模块
         .target(
             name: "BELoader",
             path: "Sources/BELoader",
-            publicHeadersPath: "include"
+            publicHeadersPath: "include" // Objective-C 头文件目录
         )
-    ],
-    swiftLanguageModes: [.v5]
+    ]
 )
